2d559f4abf760060759c67a0e41b19b7aca6366e
ankan789 <109434134+ankan789@users.noreply.github.com>
2024-09-18 16:40:12 +0530

    Update mega.py

--- mega.py
+++ mega.py
@@ -1,12 +1,10 @@
-!pip install mega.py
-
 from mega import Mega
 mega = Mega()
 m = mega.login(<user name>, <passwd>)
 
 ## file download
-file = m.find(<file name>)
-m.download(file)
+#file = m.find(<file name>)
+#m.download(file)
 
 ## file upload
-m.upload(<file name>)
+#m.upload(<file name>)
