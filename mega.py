9bf7670b2606535005b5969d26979b9516df76b8
ankan789 <109434134+ankan789@users.noreply.github.com>
2024-09-18 16:23:36 +0530

    Create mega.py
    main file

---
+++ mega.py
@@ -0,0 +1,12 @@
+!pip install mega.py
+
+from mega import Mega
+mega = Mega()
+m = mega.login(<user name>, <passwd>)
+
+## file download
+file = m.find(<file name>)
+m.download(file)
+
+## file upload
+m.upload(<file name>)
